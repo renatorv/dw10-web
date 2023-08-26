8a087ad3108d2a8a41e716b021fa2e15d9aec9b8
renatorv <renatorv@gmail.com>
2023-08-26 11:32:45 -0300

    Alterando Menu Pela URL e Historico de navegação

--- lib/src/modules/template/menu/menu_enum.dart
+++ lib/src/modules/template/menu/menu_enum.dart
@@ -24,4 +24,13 @@ enum Menu {
   final String label;
 
   const Menu(this.route, this.assetIcon, this.assetIconSelected, this.label);
+
+  static Menu? findByPath(String path) {
+    final menu = Menu.values.where((element) => path.contains(element.route));
+
+    if (menu.isNotEmpty) {
+      return menu.first;
+    }
+    return null;
+  }
 }
